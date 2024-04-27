8484d40e1528f0ab31a017475d7a626f6a271fad
SalmanRajpuat <97193516+SalmanRajpuat@users.noreply.github.com>
2024-04-27 16:16:42 +0500

    done with functions

---
+++ Functions.js
@@ -0,0 +1,19 @@
+//console.log("This is tut 53");
+
+/*function greet(name){
+    console.log(name + " is a good guy.");
+}
+
+let name = "Salman";
+let name1 = "Moaz";
+let name2 = "Junaid";
+greet(name);
+greet(name1);
+greet(name2);
+*/
+function loginUsermessage(username){
+    `${username} just logged in`
+}
+
+
+console.log(loginUsermessage("Salman"));
